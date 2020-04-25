1662054542a3e3073bd57742a815e9884071ab61
raphael <rheitjoh@mail.uni-paderborn.de>
2020-04-25 11:47:16 +0200

    Improved install_mcl script

--- install_mcl.sh
+++ install_mcl.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 mcl_version="v1.03"
 
+# check that JAVA_INC is given
+if [ $# -eq 0 ]; then
+	echo "Missing Java include argument"
+	echo "Please give path of Java include folder as first argument"
+	echo "For example: /usr/lib/jvm/java-8-openjdk-amd64/include"
+	exit 1
+fi
+
+java_inc=$1
+
 echo "----- Installing libgmp-dev -----"
 sudo apt-get update
 sudo apt-get -y install libgmp-dev # requirement for mcl
 echo "----- Cloning mcl from git://github.com/herumi/mcl -----"
 git clone git://github.com/herumi/mcl
 git checkout $mcl_version
-echo "----- Building mcl $(mcl_version) -----"
+echo "----- Building mcl $mcl_version -----"
 cd mcl || exit
-make -j4 # build mcl library
+make -j4 || exit # build mcl library
 echo "----- Building mcl java bindings -----"
-ls /usr/lib/jvm/
+echo "----- Java include path: $java_inc -----"
 cd ffi/java || exit
-make test_mcl JAVA_INC=$JAVA_INC # build java bindings, set include manually
+make test_mcl JAVA_INC=-I$java_inc # build java bindings, set include manually
 cd ../..
 echo "----- Copying mcl java shared library to /usr/lib/ -----"
 sudo cp lib/libmcljava.so /usr/lib/libmcljava.so
