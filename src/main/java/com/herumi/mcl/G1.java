22b78701292c780fb020fda928c6a4e3e3d5794f
raphael <rheitjoh@mail.uni-paderborn.de>
2020-05-30 15:07:42 +0200

    Downgrade mcl files to v1.03

--- src/main/java/com/herumi/mcl/G1.java
+++ src/main/java/com/herumi/mcl/G1.java
@@ -9,77 +9,77 @@
 package com.herumi.mcl;
 
 public class G1 {
-    private transient long swigCPtr;
-    protected transient boolean swigCMemOwn;
+  private transient long swigCPtr;
+  protected transient boolean swigCMemOwn;
 
-    protected G1(long cPtr, boolean cMemoryOwn) {
-        swigCMemOwn = cMemoryOwn;
-        swigCPtr = cPtr;
+  protected G1(long cPtr, boolean cMemoryOwn) {
+    swigCMemOwn = cMemoryOwn;
+    swigCPtr = cPtr;
+  }
+
+  protected static long getCPtr(G1 obj) {
+    return (obj == null) ? 0 : obj.swigCPtr;
+  }
+
+  @SuppressWarnings("deprecation")
+  protected void finalize() {
+    delete();
+  }
+
+  public synchronized void delete() {
+    if (swigCPtr != 0) {
+      if (swigCMemOwn) {
+        swigCMemOwn = false;
+        MclJNI.delete_G1(swigCPtr);
+      }
+      swigCPtr = 0;
     }
+  }
 
-    protected static long getCPtr(G1 obj) {
-        return (obj == null) ? 0 : obj.swigCPtr;
-    }
+  public G1() {
+    this(MclJNI.new_G1__SWIG_0(), true);
+  }
 
-    @SuppressWarnings("deprecation")
-    protected void finalize() {
-        delete();
-    }
+  public G1(G1 rhs) {
+    this(MclJNI.new_G1__SWIG_1(G1.getCPtr(rhs), rhs), true);
+  }
 
-    public synchronized void delete() {
-        if (swigCPtr != 0) {
-            if (swigCMemOwn) {
-                swigCMemOwn = false;
-                MclJNI.delete_G1(swigCPtr);
-            }
-            swigCPtr = 0;
-        }
-    }
+  public G1(Fp x, Fp y) {
+    this(MclJNI.new_G1__SWIG_2(Fp.getCPtr(x), x, Fp.getCPtr(y), y), true);
+  }
 
-    public G1() {
-        this(MclJNI.new_G1__SWIG_0(), true);
-    }
+  public boolean equals(G1 rhs) {
+    return MclJNI.G1_equals(swigCPtr, this, G1.getCPtr(rhs), rhs);
+  }
 
-    public G1(G1 rhs) {
-        this(MclJNI.new_G1__SWIG_1(G1.getCPtr(rhs), rhs), true);
-    }
+  public void set(Fp x, Fp y) {
+    MclJNI.G1_set(swigCPtr, this, Fp.getCPtr(x), x, Fp.getCPtr(y), y);
+  }
 
-    public G1(Fp x, Fp y) {
-        this(MclJNI.new_G1__SWIG_2(Fp.getCPtr(x), x, Fp.getCPtr(y), y), true);
-    }
+  public void clear() {
+    MclJNI.G1_clear(swigCPtr, this);
+  }
 
-    public boolean equals(G1 rhs) {
-        return MclJNI.G1_equals(swigCPtr, this, G1.getCPtr(rhs), rhs);
-    }
+  public void setStr(String str, int base) {
+    MclJNI.G1_setStr__SWIG_0(swigCPtr, this, str, base);
+  }
 
-    public void set(Fp x, Fp y) {
-        MclJNI.G1_set(swigCPtr, this, Fp.getCPtr(x), x, Fp.getCPtr(y), y);
-    }
+  public void setStr(String str) {
+    MclJNI.G1_setStr__SWIG_1(swigCPtr, this, str);
+  }
 
-    public void clear() {
-        MclJNI.G1_clear(swigCPtr, this);
-    }
+  public String toString(int base) {
+    return MclJNI.G1_toString__SWIG_0(swigCPtr, this, base);
+  }
 
-    public void setStr(String str, int base) {
-        MclJNI.G1_setStr__SWIG_0(swigCPtr, this, str, base);
-    }
+  public String toString() {
+    return MclJNI.G1_toString__SWIG_1(swigCPtr, this);
+  }
 
-    public void setStr(String str) {
-        MclJNI.G1_setStr__SWIG_1(swigCPtr, this, str);
-    }
+  public void deserialize(byte[] cbuf) {
+    MclJNI.G1_deserialize(swigCPtr, this, cbuf);
+  }
 
-    public String toString(int base) {
-        return MclJNI.G1_toString__SWIG_0(swigCPtr, this, base);
-    }
-
-    public String toString() {
-        return MclJNI.G1_toString__SWIG_1(swigCPtr, this);
-    }
-
-    public void deserialize(byte[] cbuf) {
-        MclJNI.G1_deserialize(swigCPtr, this, cbuf);
-    }
-
-    public byte[] serialize() { return MclJNI.G1_serialize(swigCPtr, this); }
+  public byte[] serialize() { return MclJNI.G1_serialize(swigCPtr, this); }
 
 }
--- src/main/java/com/herumi/mcl/MclConstants.java
+++ src/main/java/com/herumi/mcl/MclConstants.java
@@ -9,6 +9,6 @@
 package com.herumi.mcl;
 
 public interface MclConstants {
-    public final static int BN254 = 0;
-    public final static int BLS12_381 = 5;
+  public final static int BN254 = 0;
+  public final static int BLS12_381 = 5;
 }
