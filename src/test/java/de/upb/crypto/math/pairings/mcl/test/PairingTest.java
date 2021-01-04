c4441d394aaf4cac6122dd1349f4eeed108ea709
raphael <rheitjoh@mail.uni-paderborn.de>
2021-01-04 10:45:56 +0100

    Add lazy wrapper class for Mcl bilinear group and remove performance test (now in benchmarks project)

--- src/test/java/de/upb/crypto/math/pairings/mcl/test/PairingTest.java
+++ src/test/java/de/upb/crypto/math/pairings/mcl/test/PairingTest.java
@@ -1,10 +1,8 @@
 package de.upb.crypto.math.pairings.mcl.test;
 
-import de.upb.crypto.math.pairings.generic.BilinearGroup;
 import de.upb.crypto.math.pairings.generic.BilinearMap;
-import de.upb.crypto.math.pairings.mcl.MclBilinearGroupImpl;
+import de.upb.crypto.math.pairings.mcl.MclBilinearGroup;
 import de.upb.crypto.math.pairings.test.PairingTests;
-import de.upb.crypto.math.structures.groups.lazy.LazyBilinearGroup;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -20,7 +18,7 @@ public class PairingTest extends PairingTests {
     @Parameterized.Parameters(name = "Test: {0}")
     public static Collection<BilinearMap[]> data() {
         BilinearMap[][] params = new BilinearMap[][]{{
-                new LazyBilinearGroup(new MclBilinearGroupImpl()).getBilinearMap()
+                new MclBilinearGroup().getBilinearMap()
         }};
         return Arrays.asList(params);
     }
