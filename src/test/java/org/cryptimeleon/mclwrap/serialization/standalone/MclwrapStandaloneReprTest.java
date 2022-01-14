f27fe5b91bc3fce4ffd566681a3d03978805c267
Jan Bobolz <mail@jan-bobolz.de>
2022-01-14 20:47:55 +0100

    Fix compile error

--- src/test/java/org/cryptimeleon/mclwrap/serialization/standalone/MclwrapStandaloneReprTest.java
+++ src/test/java/org/cryptimeleon/mclwrap/serialization/standalone/MclwrapStandaloneReprTest.java
@@ -1,9 +1,16 @@
 package org.cryptimeleon.mclwrap.serialization.standalone;
 
 import org.cryptimeleon.math.serialization.standalone.StandaloneReprTest;
+import org.cryptimeleon.mclwrap.bn254.MclBilinearGroup;
 
 public class MclwrapStandaloneReprTest extends StandaloneReprTest {
     public MclwrapStandaloneReprTest() {
         super("org.cryptimeleon.mclwrap");
+    }
+
+    @Override
+    public void testStandaloneRepresentablesWithParameterlessConstructors() {
+        MclBilinearGroup.resetMclInitializationAndAcceptErrors();
+        super.testStandaloneRepresentablesWithParameterlessConstructors();
     }
 }
